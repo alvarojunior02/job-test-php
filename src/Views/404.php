8dab3ee35a44873f52b3290a3763ad5a8067fc51
Alvaro Junior <alvaro.netpixel@gmail.com>
2024-03-10 18:18:00 -0400

    feat: add 404 not found view

---
+++ src/Views/404.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="stylesheet" href="css/global.css">
+  <link rel="stylesheet" href="css/header.css">
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
+
+  <title>Loja Mágica - 404 - Não encontrado</title>
+</head>
+
+<body style="max-width: 100vw; max-height: 100vh; max-width: 100vw; overflow: hidden">
+  <header>
+    <div id="header-content">
+      <div id="header-logo">
+        <h1 style="color: #FFFFFF">Loja Mágica</h1>
+      </div>
+      <div id="header-links">
+        <a href="/" style="margin-right: 20px">Home</a>
+        <a href="/clientes">Clientes</a>
+        <a href="/pedidos" style="margin-left: 20px">Pedidos</a>
+      </div>
+    </div>
+  </header>
+  <div style="
+      display: flex; 
+      flex-direction: column; 
+      justify-content: center; 
+      align-items: center; 
+      color: #166534;
+      width: 100vw; 
+      height: 100vh;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    ">
+    <h1 style="font-size: 60px">404</h1>
+    <p>Página não encontrada</p>
+  </div>
+</body>
+
+</html>
