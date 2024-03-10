01c341dee640660fd0d841a57b4a084cd5ab0144
Alvaro Junior <alvaro.netpixel@gmail.com>
2024-03-10 18:18:41 -0400

    feat: remove css in tag style and standardize header

--- src/Views/customer/index.php
+++ src/Views/customer/index.php
@@ -5,6 +5,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+  <link rel="stylesheet" href="css/global.css">
+  <link rel="stylesheet" href="css/header.css">
   <link rel="stylesheet" href="css/customers.css">
 
   <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -12,43 +14,13 @@
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
 
   <title>Loja Mágica - Clientes</title>
-
-  <style>
-    * {
-      margin: 0;
-      padding: 0;
-
-      font-family: "Inter", sans-serif;
-    }
-
-    body {
-      background-color: #FFFFFF;
-
-      margin: 0;
-      padding: 0;
-
-      display: flex;
-      flex-direction: column;
-      justify-content: flex-start;
-      align-items: center;
-
-      max-width: 100vw;
-      width: 100vw;
-      max-height: 100vh;
-      height: 100vh;
-      /* overflow: hidden; */
-    }
-  </style>
 </head>
 
 <body>
   <header>
     <div id="header-content">
       <div id="header-logo">
-        <div id="header-logo-card">
-          <img src="../../assets/icons/customers.png" width="50" height="50" />
-          <p>Clientes</p>
-        </div>
+        <h1 style="color: #FFFFFF">Loja Mágica</h1>
       </div>
       <div id="header-links">
         <a href="/" style="margin-right: 20px">Home</a>
