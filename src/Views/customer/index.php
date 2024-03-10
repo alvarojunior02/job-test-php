556636cff29195fe7d812d8af794a745f559d52a
Alvaro Junior <alvaro.netpixel@gmail.com>
2024-03-10 17:33:34 -0400

    feat: add import csv form and customers table in view/customers/index.php

--- src/Views/customer/index.php
+++ src/Views/customer/index.php
@@ -62,22 +62,56 @@
     <div>
       <form id="form-import-customers" method="POST" action="/clientes/import" enctype="multipart/form-data">
         <div>
-          <label for="file_csv">Importar Clientes (.csv)</label>
-          <input type="file" name="file_csv" id="file_csv" required />
+          <label for="file-upload-button">
+            Importar Clientes (.csv)
+            <input type="file" id="file-upload-button" name="file_csv" accept="text/csv" required />
+          </label>
+          <span id="file-upload-name"></span>
         </div>
-        <button type="submit">Enviar</button>
+        <button id="form-submit-button" type="submit" disabled>Enviar</button>
+
+        <script>
+          let input = document.getElementById("file-upload-button");
+          let uploadName = document.getElementById("file-upload-name");
+          let uploadButton = document.getElementById("form-submit-button");
+
+          input.addEventListener("change", () => {
+            let inputImage = document.querySelector("input[type=file]").files[0];
+            uploadName.innerText = inputImage.name;
+            uploadButton.removeAttribute('disabled');
+          })
+        </script>
       </form>
 
+      <?php
+      if (isset($_SESSION['imported_csv_message'])) {
+        echo '
+            <br>
+            <span>' . $_SESSION['imported_csv_message'] . '</span>
+          ';
+        unset($_SESSION['imported_csv_message']);
+      }
+      ?>
+
       <table id="customers-list">
-        <tr>
-          <th>ID</th>
+        <tr style="background-color: #166534; color: #FFFFFF">
           <th>CPF</th>
           <th>Nome</th>
           <th>E-mail</td>
           <th>Ações</th>
         </tr>
         <?php
-        echo $customers;
+        if (isset($_SESSION['data']['customers']) && is_array($_SESSION['data']['customers'])) {
+          foreach ($_SESSION['data']['customers'] as $customer) {
+            $html = '<tr>';
+            $html .= '<td class="center">' . $customer['cpf'] . '</td>';
+            $html .= '<td>' . $customer['name'] . '</td>';
+            $html .= '<td>' . $customer['email'] . '</td>';
+            $html .= '<td class="center"> <a href="/clientes/' . $customer['id'] . '"> Ver </a></td>';
+            $html .= '</tr>';
+            echo $html;
+          }
+        }
         ?>
       </table>
     </div>
