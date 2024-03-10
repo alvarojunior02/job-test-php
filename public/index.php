6627a5ea6dc0d717fd060cb1d7307fd5e1cae589
Alvaro Junior <alvaro.netpixel@gmail.com>
2024-03-10 17:32:26 -0400

    feat session_start and load dotenv in index.php

--- public/index.php
+++ public/index.php
@@ -2,4 +2,10 @@
 
 require '../vendor/autoload.php';
 
+session_start();
+
+// Carrega as variáveis do .env para o $_ENV com o pacote Dotenv
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__, 1));
+$dotenv->load();
+
 $router = require '../src/Routes/index.php';
