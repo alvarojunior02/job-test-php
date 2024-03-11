5e00619ae5122c0eaf7e97640450d3064870e1b0
Alvaro Junior <alvaro.netpixel@gmail.com>
2024-03-11 12:29:08 -0400

    feat: add functions to manipulate customers

--- src/Controllers/CustomerController.php
+++ src/Controllers/CustomerController.php
@@ -3,7 +3,10 @@
 namespace App\Controllers;
 
 use App\Controller;
+use App\Mailer;
 use App\Models\Customer;
+use App\Models\Email;
+use App\Models\Order;
 use Exception;
 
 class CustomerController extends Controller
@@ -18,6 +21,29 @@ class CustomerController extends Controller
     $this->render(view: 'customer/index', data: $data);
   }
 
+  public function show()
+  {
+    try {
+      if (isset($_REQUEST['id'])) {
+        $customer = Customer::findOne($_REQUEST['id']);
+        $orders = Order::findManyByCustomerId($_REQUEST['id']);
+        $emails = Email::findManyByCustomerId($_REQUEST['id']);
+        $data = [
+          'customer' => $customer,
+          'orders' => $orders,
+          'emails' => $emails
+        ];
+
+        $this->render(view: 'customer/show', data: $data);
+      } else {
+        throw new Exception('ID do cliente não encontrado!');
+      }
+      die();
+    } catch (Exception $e) {
+      echo 'Erro ao visualizar cliente: ' . $e->getMessage();
+    }
+  }
+
   public function importCsv()
   {
     try {
@@ -28,7 +54,7 @@ class CustomerController extends Controller
       }
 
       if ($file['type'] != "text/csv") { // Verifica se o tipo é .csv
-        throw new Exception('Arquivo não permitido!');
+        throw new Exception('Tipo de arquivo não permitido!');
       }
 
       $file_data = fopen($file['tmp_name'], "r");
@@ -55,6 +81,155 @@ class CustomerController extends Controller
       header("Location: /clientes");
     } catch (Exception $e) {
       echo 'Erro ao importar planilha (.csv): ' . $e->getMessage();
+    }
+  }
+
+  public function create()
+  {
+    $this->render(view: 'customer/create');
+  }
+
+  private function validateCpf($cpf)
+  {
+    // Extrai somente os números
+    $cpf = preg_replace('/[^0-9]/is', '', $cpf);
+
+    // Verifica se foi informado todos os digitos corretamente
+    if (strlen($cpf) != 11) {
+      return false;
+    }
+
+    // Verifica se foi informada uma sequência de digitos repetidos. Ex: 111.111.111-11
+    if (preg_match('/(\d)\1{10}/', $cpf)) {
+      return false;
+    }
+
+    // Faz o calculo para validar o CPF
+    for ($t = 9; $t < 11; $t++) {
+      for ($d = 0, $c = 0; $c < $t; $c++) {
+        $d += $cpf[$c] * (($t + 1) - $c);
+      }
+      $d = ((10 * $d) % 11) % 10;
+      if ($cpf[$c] != $d) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  public function store()
+  {
+    try {
+      if (isset($_POST['cpf']) && isset($_POST['name']) && isset($_POST['email'])) {
+        if (!$this->validateCpf($_POST['cpf'])) {
+          throw new Exception("CPF inválido!");
+        }
+
+        $customer = new Customer($_POST['cpf'], $_POST['name'], $_POST['email']);
+        $customer->store();
+        $_SESSION['crud_message'] = "Novo cliente cadastrado com sucesso!";
+        header("Location: /clientes");
+      } else {
+        throw new Exception("Não pode ter campos em branco!");
+      }
+    } catch (Exception $e) {
+      echo "Erro ao cadastrar novo cliente: " . $e->getMessage();
+    }
+  }
+
+  public function edit()
+  {
+    try {
+      if (isset($_REQUEST['id'])) {
+        $customer = Customer::findOne($_REQUEST['id']);
+        $data = [
+          'customer' => $customer
+        ];
+
+        $this->render(view: 'customer/edit', data: $data);
+      } else {
+        throw new Exception('ID do cliente não encontrado!');
+      }
+      die();
+    } catch (Exception $e) {
+      echo 'Erro ao editar cliente: ' . $e->getMessage();
+    }
+  }
+
+  public function update()
+  {
+    try {
+      if (isset($_POST['name']) && isset($_POST['email'])) {
+        Customer::update(id: intval($_REQUEST['id']), name: $_POST['name'], email: $_POST['email']);
+
+        $_SESSION['crud_message'] = "Cliente alterado com sucesso!";
+        header("Location: /clientes");
+      } else {
+        throw new Exception("Não pode ter campos em branco!");
+      }
+    } catch (Exception $e) {
+      echo "Erro ao editar cliente: " . $e->getMessage();
+    }
+  }
+
+  public function create_email()
+  {
+    try {
+      if (isset($_REQUEST['id'])) {
+        $customer = Customer::findOne($_REQUEST['id']);
+        $data = array(
+          'customer' => $customer
+        );
+
+        $this->render(view: 'customer/create_email', data: $data);
+      } else {
+        throw new Exception('ID do cliente não encontrado');
+      }
+    } catch (Exception $e) {
+      echo 'Erro ao exibir tela de envio de e-mail: ' > $e->getMessage();
+    }
+  }
+
+  public function send_email()
+  {
+    try {
+      if (isset($_POST['customer_id']) || isset($_POST['subject']) || isset($_POST['message'])) {
+        $customer = Customer::findOne($_POST['customer_id']);
+
+        $mailer = new Mailer();
+        $mailer->setRecipient(email: $customer['email'], name: $customer['name']);
+        $mailer->setContent(subject: $_POST['subject'], body: nl2br($_POST['message']));
+        $mailer->send();
+
+        $email = new Email($_POST['customer_id'], $_POST['subject'], nl2br($_POST['message']));
+        $email->store();
+
+        $_SESSION['email_success'] = "E-mail enviado com sucesso!";
+        header("Location: /clientes/info?id=" . $_POST['customer_id']);
+      } else {
+        throw new Exception("Estão faltando informações para compor o e-mail!");
+      }
+    } catch (Exception $e) {
+      echo 'Erro ao enviar e-mail: ' . $e->getMessage();
+    }
+  }
+
+  public function show_email()
+  {
+    try {
+      $email = Email::findOne($_REQUEST['id']);
+
+      $customer = Customer::findOne($email['customer_id']);
+
+      $data = array(
+        'email' => $email,
+        'customer' => $customer
+      );
+
+      $this->render(view: 'customer/show_email', data: $data);
+    } catch (Exception $e) {
+      echo 'Erro ao buscar informações do e-mail: ' . $e->getMessage();
     }
   }
 }
